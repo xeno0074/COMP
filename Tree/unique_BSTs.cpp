2e73e9f346694292077e59766c7a55b9ecc7e1e6
Arif Ahmed <arixx74@gmail.com>
2020-09-15 22:23:14 +0530

    added unique bsts generation

--- Tree/unique_BSTs.cpp
+++ Tree/unique_BSTs.cpp
@@ -1,21 +1,46 @@
 #include"tree.h"
 
-vector<Node *> T(int n, int s){
-    vector<vector<Node *>> forest(n + 1);
-    vector<Node *> temp;
+vector<vector<Node *>> forest;
 
-    //  insert item for forest[0]
-    temp.push_back(NULL);
-    forest[0] = temp;
-
-    for(int index = 1; index <= n; index++){
-        for(int rootNum = 1; rootNum <= index; rootNum++){
-            
-        }
+vector<Node *> FetchSubTrees(int n, int s){
+    vector<Node *> row;
+    if(!n){
+        row.push_back(NULL);
+        return row;
     }
-    
+    for(Node *root : forest[n]){
+        row.push_back(DuplicateTree(root, s));
+    }
+    return row;
 }
 
 vector<Node *> GenUniqueBSTs(int n){
-    
+    forest.resize(n + 1);
+    vector<Node *> row;
+
+    //  insert item for forest[0]
+    row.push_back(NULL);
+    forest[0] = row;
+    row.clear();
+
+    for(int index = 1; index <= n; index++){
+        for(int rootNum = 1; rootNum <= index; rootNum++){
+            vector<Node *> leftSubTrees = FetchSubTrees(rootNum - 1, 0);
+            vector<Node *> rightSubTrees = FetchSubTrees(index - rootNum, rootNum);
+            for(Node *lNode : leftSubTrees){
+                for(Node *rNode :rightSubTrees){
+                    Node *node = new Node(rootNum);
+                    node->left = lNode;
+                    node->right = rNode;
+                    forest[index].push_back(node);
+                }
+            }
+        }
+    }
+    return FetchSubTrees(n, 0);
+}
+
+int main(){
+    auto t = GenUniqueBSTs(3);
+    for(auto x : t) cout << ToString(x) << endl;
 }
